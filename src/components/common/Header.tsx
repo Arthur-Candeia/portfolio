12c6f17ebdda329f84e50b189181bcda45c7ce51
Arthur-Candeia <arthurheher@hotmail.com>
2024-04-12 15:00:58 -0300

    FEAT: Finalização da página de orçamentos.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,6 +5,25 @@ import Link from "next/link";
 import styles from "@/styles/components/common/header.module.scss"
 
 export function Header() {
+
+  return (
+    <header className={styles.header}>
+      <Link href="/" target="_self" rel="prev" className={styles.name}>Arthur Candeia</Link>
+      <nav className={styles.navDesktop}>
+        <Links />
+      <Link href="https://api.whatsapp.com/send?phone=5527998642163&text=Olá!%20Tenho%20um%20projeto!" target="_blank" rel="external" className={styles.chat}>
+        Vamos conversar {'>'}
+      </Link>
+      </nav>
+      <BurgerMenu />
+      <nav className="mobile-nav">
+        <Links />
+      </nav>
+    </header>
+  )
+}
+
+function Links() {
   const pathname = usePathname()
   const links = [
     {href: '/orcamentos', name: 'Orçamentos'},
@@ -12,26 +31,44 @@ export function Header() {
     {href: '/recomendacoes', name: 'Recomendações'}
   ]
 
-  console.log(pathname)
+  function handleClose() {
+    const nav = document.querySelector('.menu-burguer')
+    nav!.parentElement?.querySelector('.mobile-nav')?.classList.remove('show-mobile-nav')
+    nav!.classList.remove('open')
+  }
+
   return (
-    <header className={styles.header}>
-      <Link href="/" target="_self" rel="prev" className={styles.name}>Arthur Candeia</Link>
-      <nav>
-        <Link href="/" target="_self" rel="prev" className={pathname === '/' ? styles.active : ''}>Início</Link>
-        {links.map(({href, name}, index) => (
-          <Link
-            href={href}
-            key={index}
-            target="_self" rel="next"
-            className={pathname.includes(href) ? styles.active : ''}
-          >
-            {name}
-          </Link>
-        ))}
-      <Link href="https://api.whatsapp.com/send?phone=5527998642163&text=Olá!%20Tenho%20um%20projeto!" target="_blank" rel="external" className={styles.chat}>
-        Vamos conversar {'>'}
+    <>
+      <Link href="/" target="_self" rel="prev" className={pathname === '/' ? styles.active : ''} onClick={handleClose}>
+        Início
       </Link>
-      </nav>
-    </header>
+      {links.map(({href, name}, index) => (
+        <Link
+          href={href}
+          key={index}
+          target="_self" rel="next"
+          className={pathname.includes(href) ? styles.active : ''}
+          onClick={handleClose}
+        >
+          {name}
+        </Link>
+      ))}
+    </>
+  )
+}
+
+function BurgerMenu() {
+
+  function handleClick(ev: React.MouseEvent) {
+    ev.currentTarget.parentElement?.querySelector('.mobile-nav')?.classList.toggle('show-mobile-nav')
+    ev.currentTarget.classList.toggle('open')
+  }
+
+  return (
+    <article className="menu-burguer" onClick={handleClick}>
+      <div className="menu-line"></div>
+      <div className="menu-line"></div>
+      <div className="menu-line"></div>
+    </article>
   )
 }
--- src/components/orcamentos/Form.tsx
+++ src/components/orcamentos/Form.tsx
@@ -1,21 +1,46 @@
 'use client'
 
+import { FormEvent, useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import styles from '@/styles/orcamentos.module.scss'
 
 export function Form() {
+  const nameRef = useRef<HTMLInputElement>(null)
+  const telRef = useRef<HTMLInputElement>(null)
+  const typeRef = useRef<HTMLSelectElement>(null)
+  const messageRef = useRef<HTMLTextAreaElement>(null)
+  const submitRef = useRef<HTMLInputElement>(null)
+
+  function handleSubmit(ev: FormEvent) {
+    ev.preventDefault()
+    const params = {
+      from_name: nameRef.current!.value,
+      tel: telRef.current!.value,
+      type: typeRef.current!.value,
+      message: messageRef.current!.value
+    }
+
+    emailjs.send(process.env.NEXT_PUBLIC_SERVICE_ID!, process.env.NEXT_PUBLIC_TEMPLATE_ID!, params as any, process.env.NEXT_PUBLIC_KEY!)
+    const inputSubmit = submitRef.current!
+    inputSubmit.value = 'Orçamento Enviado!'
+    inputSubmit.style.color = 'white'
+    inputSubmit.style.backgroundColor = 'transparent'
+    inputSubmit.style.outlineColor = '#00ff88'
+  }
+
   return (
-    <form autoComplete="on" method="POST" spellCheck={false}>
+    <form autoComplete="on" method="POST" spellCheck={false} onSubmit={handleSubmit}>
       <div>
         <label htmlFor="name">Nome:</label>
-        <input type="text" id='name' className={styles.inputName} placeholder="Qual o seu nome?" required minLength={4} />
+        <input type="text" id='name' className={styles.inputName} placeholder="Qual o seu nome?" required minLength={4} ref={nameRef}/>
       </div>
       <div>
         <label htmlFor="tel">WhatsApp:</label>
-        <input type="tel" className={styles.tel} placeholder="(XX) XXXXX-XXXX" required minLength={8} />
+        <input type="tel" className={styles.tel} placeholder="(XX) XXXXX-XXXX" required minLength={8} ref={telRef} />
       </div>
       <div>
         <label htmlFor="type">Escolha o tipo de projeto:</label>
-        <select id="type" className={styles.type} defaultValue="" required>
+        <select id="type" className={styles.type} defaultValue="" required ref={typeRef}>
           <option value="" disabled>Selecione uma opção</option>
           <option value="app">Desenvolvimento de aplicativo (Android / IOS)</option>
           <option value="desktop">Desenvolvimento de software desktop</option>
@@ -31,9 +56,9 @@ export function Form() {
       </div>
       <div>
         <label htmlFor="message">Descreva seu projeto:</label>
-        <textarea id='message' className={styles.message} cols={30} rows={7} placeholder="Digite aqui sua mensagem!" required minLength={4}></textarea>
+        <textarea id='message' className={styles.message} cols={30} rows={7} placeholder="Digite aqui sua mensagem!" required minLength={4} ref={messageRef}></textarea>
       </div>
-      <input type="submit" value="ENVIAR" />
+      <input type="submit" value="ENVIAR" ref={submitRef} />
     </form>
   )
 }
